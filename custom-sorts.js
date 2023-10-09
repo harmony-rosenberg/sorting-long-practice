a16fd380e75b9bb1dc2d94f108ed3fff8852c138
harmony <harmony.rosenberg94@gmail.com>
2023-10-09 18:05:08 -0500

    problem 3 solved

--- custom-sorts.js
+++ custom-sorts.js
@@ -19,7 +19,27 @@ function oddEvenSort(arr) {
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  if(s.length !== t.length) return false
+
+  let charCount = {
+  }
+
+  for(let char of s) {
+    if(!charCount[char]) {
+      charCount[char] = 1
+    } else {
+      charCount[char] += 1
+    }
+  }
+
+  for(let char of t) {
+    if(!charCount[char]) return false
+    charCount[char]--
+  }
+
+  for(let char in charCount) {
+    if (charCount[char] === 0) return true
+  }
 }
 
 function reverseBaseSort(arr) {
